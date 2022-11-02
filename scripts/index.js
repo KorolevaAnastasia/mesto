91b97100c9776c76c518c99702cdfbcb42e382ee
Anastasia Koroleva <a.koroleva@loftice.ru>
2022-11-02 18:03:32 +0300

    hotfix/all: всем кнопкам добавлен атрибут aria-label hotfix/popup: убрала фиксированное позиционирование для popup контейнера, переделала на flex hotfix/profile: сделано выравнивание контента внутри блока, переделаны кнопки "добавить" и "изменить" hotfix/popup: переписана форма и поправлены отступы hotfix/fronts: удалила случайный шрифт Inter-MediumItalic hotfix/js: добавлены переменные, переписаны функции, файл перенесен в папку scripts hotfix/buttons: убрала черную заливку у кнопок, теперь прозрачная hotfix/footer: прижат к низу экрана

---
+++ scripts/index.js
@@ -0,0 +1,36 @@
+let closeButton = document.querySelector('.popup__close-button');
+let profileEditButton = document.querySelector('.profile__edit-button');
+let popup = document.querySelector('.popup');
+let formElement = document.querySelector('.popup__form');
+let nameInput = document.querySelector('.popup__form-text_type_name');
+let jobInput = document.querySelector('.popup__form-text_type_job');
+let nameText = document.querySelector('.profile__name');
+let jobText = document.querySelector('.profile__job');
+
+closeButton.addEventListener('click', popupClosed);
+profileEditButton.addEventListener('click', popupOpened);
+formElement.addEventListener('submit', formSubmitHandler);
+
+function formSubmitHandler (evt) {
+  evt.preventDefault();
+  inputProfileText();
+  popupClosed();
+}
+
+function popupOpened () {
+  popup.classList.add('popup_opened');
+  inputProfileText();
+}
+
+function popupClosed () {
+  popup.classList.remove('popup_opened');
+}
+
+function inputProfileText(){
+  nameText.textContent = nameInput.value;
+  jobText.textContent = jobInput.value;
+
+}
+
+
+
