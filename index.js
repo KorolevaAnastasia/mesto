f29d4c400ee7eaa8b8d98867534a37aba5f2ceed
Anastasia Koroleva <a.koroleva@loftice.ru>
2022-11-02 03:00:41 +0300

    hotfix/popup: убрала фиксированное позиционирование для popup контейнера, переделала на flex hotfix/profile: исправлена проблема, когда плыла верстка при переполнении профиля текстом hotfix/fronts: убраны лишние шрифты, добавлен medium hotfix/js: переписала названия переменных и функций с lower_case на camelCase hotfix/buttons: убрала черную заливку у кнопок, теперь прозрачная

--- index.js
+++ index.js
@@ -1,26 +1,26 @@
-let close_button = document.querySelector('.popup__close-button');
-let profile_edit_button = document.querySelector('.profile__edit-button');
+let closeButton = document.querySelector('.popup__close-button');
+let profileEditButton = document.querySelector('.profile__edit-button');
 let popup = document.querySelector('.popup');
-let form_element = document.querySelector('.popup');
-let name_input = document.querySelector('.popup__name-input');
-let job_input = document.querySelector('.popup__description-input');
+let formElement = document.querySelector('.popup');
+let nameInput = document.querySelector('.popup__name-input');
+let jobInput = document.querySelector('.popup__description-input');
 
-close_button.addEventListener('click', function () {
+closeButton.addEventListener('click', function () {
   popup.classList.remove('popup_opened');
 });
 
-profile_edit_button.addEventListener('click', function () {
+profileEditButton.addEventListener('click', function () {
   popup.classList.add('popup_opened');
 });
 
 function formSubmitHandler (evt) {
   evt.preventDefault();
-  document.querySelector('.profile__name').textContent = name_input.value;
-  document.querySelector('.profile__description').textContent = job_input.value;
+  document.querySelector('.profile__name').textContent = nameInput.value;
+  document.querySelector('.profile__description').textContent = jobInput.value;
 }
 
-form_element.addEventListener('submit', formSubmitHandler);
-form_element.addEventListener('submit', function () {
+formElement.addEventListener('submit', formSubmitHandler);
+formElement.addEventListener('submit', function () {
   popup.classList.remove('popup_opened');
 });
 
